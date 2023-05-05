f64139151f7cd7dcbd55a4021b338538b9e0769a
Andrey <YtrikPlay@gmail.com>
2023-05-06 00:56:30 +0300

    Все метафайлы, плюс healthbar Антона

---
+++ Assets/Textures for menu/Scripts/CollisionDamage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour
+{
+    public int collisionDamage = 10;
+    public string collisionTag;
+
+    private void OnCollisionEnter2D(Collision2D coll)       //ф-я коллизия
+    {
+        if (coll.gameObject.tag == collisionTag)
+        {
+            HealthBar healthBar = coll.gameObject.GetComponent<HealthBar>();
+            PlayerController playerController = coll.gameObject.GetComponent<PlayerController>();
+            //fill = health - damage
+
+
+            healthBar.fill = playerController.health / 100;
+        }
+
+    }
+
+}
